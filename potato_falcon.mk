b6fcd149fef15291826624721c7f1f427978ff04
hbarsaiyan <37732050+hbarsaiyan@users.noreply.github.com>
2020-01-10 13:49:33 +0530

    Update and rename superior_falcon.mk to potato_falcon.mk

--- potato_falcon.mk
+++ potato_falcon.mk
@@ -1,11 +1,11 @@
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
- # Inherit some common SuperiorOS stuff.
-$(call inherit-product, vendor/superior/config/common.mk)
+ # Inherit some common potato stuff.
+$(call inherit-product, vendor/potato/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 720
 
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := superior_falcon
+PRODUCT_NAME := potato_falcon
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
