d69080cbf4373910b1623d888b5ba0fe7011bcb4
hbarsaiyan <37732050+hbarsaiyan@users.noreply.github.com>
2020-01-10 22:43:56 +0530

    Update and rename superior_falcon.mk to revengeos_falcon.mk

--- revengeos_falcon.mk
+++ revengeos_falcon.mk
@@ -1,11 +1,11 @@
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
  # Inherit some common SuperiorOS stuff.
-$(call inherit-product, vendor/superior/config/common.mk)
+$(call inherit-product, vendor/revengeos/config/common.mk)
 
 TARGET_BOOT_ANIMATION_RES := 720
 
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := superior_falcon
+PRODUCT_NAME := revengeos_falcon
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
