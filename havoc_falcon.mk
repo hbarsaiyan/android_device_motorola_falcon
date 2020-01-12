a4b9c0452a1f3f1b83297dd732d0cd31a898d34a
hbarsaiyan <37732050+hbarsaiyan@users.noreply.github.com>
2020-01-13 03:49:16 +0530

    Update and rename lineage_falcon.mk to havoc_falcon.mk

--- havoc_falcon.mk
+++ havoc_falcon.mk
@@ -1,9 +1,11 @@
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+
+TARGET_BOOT_ANIMATION_RES := 720
 
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := lineage_falcon
+PRODUCT_NAME := havoc_falcon
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
