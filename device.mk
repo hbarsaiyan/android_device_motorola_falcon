590f68231f54b60675da944cbfcc1c633557d9d2
hbarsaiyan <37732050+hbarsaiyan@users.noreply.github.com>
2020-02-06 19:01:41 +0000

    posp: product uses qcom hardware

--- device.mk
+++ device.mk
@@ -16,6 +16,7 @@
 
 # call the proprietary setup
 $(call inherit-product, vendor/motorola/falcon/falcon-vendor.mk)
+PRODUCT_USES_QCOM_HARDWARE := true
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
