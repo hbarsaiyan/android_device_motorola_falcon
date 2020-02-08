1c0f4a997f9859609eab9551892a42684254e213
hbarsaiyan <37732050+hbarsaiyan@users.noreply.github.com>
2020-02-08 15:22:35 +0000

    Update aosip_falcon.mk

--- aosip_falcon.mk
+++ aosip_falcon.mk
@@ -6,6 +6,6 @@ $(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
 
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := arrow_falcon
+PRODUCT_NAME := aosip_falcon
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
