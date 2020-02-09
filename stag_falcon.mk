8c74ebae6abcaf7fcae9d0a19fd8cd027c9e091e
hbarsaiyan <hbarsaiyan@gmail.com>
2020-02-09 21:44:39 +0000

    Stag-ify

--- stag_falcon.mk
+++ stag_falcon.mk
@@ -1,6 +1,17 @@
-$(call inherit-product, device/motorola/falcon/full_falcon.mk)
-
+# Inherit some StagOS stuff
 $(call inherit-product, vendor/stag/main.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from falcon device
+$(call inherit-product, device/motorola/falcon/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := falcon
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := falcon
+PRODUCT_MANUFACTURER := motorola
 
 TARGET_BOOT_ANIMATION_RES := 720
 
