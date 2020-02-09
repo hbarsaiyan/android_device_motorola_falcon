f83ef86344fedf12d420da2da05a88f9624db4d7
hbarsaiyan <37732050+hbarsaiyan@users.noreply.github.com>
2020-02-09 14:44:55 +0000

    Update arrow_falcon.mk

--- arrow_falcon.mk
+++ arrow_falcon.mk
@@ -1,11 +1,10 @@
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/stag/main.mk)
 
 TARGET_BOOT_ANIMATION_RES := 720
 
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := arrow_falcon
+PRODUCT_NAME := stag_falcon
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
